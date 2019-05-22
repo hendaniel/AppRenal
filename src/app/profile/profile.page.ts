c4bcc2ee226ff4ae21839d7ed41c6a9296265230
Edward-Acer <edwardbaron99@gmail.com>
2019-05-22 05:18:08 -0500

    Cambios minimos xd

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,57 +1,55 @@
-import { Component, OnInit, OnDestroy, DoCheck, OnChanges, SimpleChanges, AfterViewInit, AfterViewChecked, AfterContentInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { user } from "../models/user";
 import { UserService } from "../Services/user.service";
 import { NavController } from "@ionic/angular";
-import { ProductService } from '../Services/product.service';
-import { Historia } from '../models/historia';
-import { Propiedad } from '../models/propiedad';
+import { ProductService } from "../Services/product.service";
+import { Historia } from "../models/historia";
+import { Propiedad } from "../models/propiedad";
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.page.html",
   styleUrls: ["./profile.page.scss"]
 })
-export class ProfilePage implements OnInit{
-
+export class ProfilePage implements OnInit {
   show: boolean;
   usuario: user;
   historias: Historia[];
   propiedades: Propiedad[];
-  constructor(private userServices: UserService,
-     private nav: NavController,
-     private productServices: ProductService) {
-       this.usuario = new user();
-       this.usuario.propiedades = new Array();
-       this.historias = new Array();
-
+  constructor(
+    private userServices: UserService,
+    private nav: NavController,
+    private productServices: ProductService
+  ) {
+    this.usuario = new user();
+    this.historias = new Array();
+    this.usuario.dietas = new Array();
+    this.usuario = this.userServices.getUser();
+    this.historias = this.userServices.getHistorias();
+    this.propiedades = this.productServices.getNamesPropiedades();
     this.show = false;
     console.log(this.usuario);
   }
 
   ngOnInit() {
+    console.log("INICIA");
     setTimeout(() => {
       this.show = true;
     }, 2000);
-
-    console.log("INICIA");
   }
-  
-  ionViewWillEnter(){
+
+  ionViewWillEnter() {
     this.usuario = this.userServices.getUser();
     this.historias = this.userServices.getHistorias();
     this.propiedades = this.productServices.getNamesPropiedades();
     console.log(this.usuario.indexPerfil);
+    console.log("Entra en perfil usuario.");
+    
   }
 
-  masInfo(){
-    this.nav.navigateForward(['/more-info-profile']);
+  masInfo() {
+    this.nav.navigateForward(["/more-info-profile"]);
   }
-
-  ngOnDestroy(){
-    console.log("Finaliza");
-  }
-
-
 
   logout() {
     console.log("logout Clicked");
@@ -63,7 +61,7 @@ export class ProfilePage implements OnInit{
     this.nav.navigateRoot("/login");
   }
 
-  mostrar(){
+  mostrar() {
     return this.usuario.propiedades.length > 0;
   }
 }
